66edc395c7ddc983bf79422b6da0228573900a2b
Enes Tufekci <enesxtufekci@gmail.com>
2024-04-12 13:14:04 +0200

    config fix

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,5 +5,5 @@ export type EnvConfigType = {
 
 export const envConfig: EnvConfigType = {
   apiBaseUrl: process.env.NEXT_PUBLIC_API_URL,
-  apiBasePrefix: `/configutrator/${process.env.NEXT_PUBLIC_API_PREFIX}`,
+  apiBasePrefix: `/configurator/${process.env.NEXT_PUBLIC_API_PREFIX}`,
 };
